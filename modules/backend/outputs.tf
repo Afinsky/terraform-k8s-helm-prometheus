4ed2c619665191aa2392bbede3482079dc109d73
Aliaksei Batsian <aliaksei@Aliakseis-MacBook-Pro.local>
2026-08-06 14:59:56 +0200

    fix: backend has been updated

--- modules/backend/outputs.tf
+++ modules/backend/outputs.tf
@@ -1,11 +1,14 @@
 output "terraform_state_bucket_id" {
-  value = module.s3.s3_bucket_id #aws_s3_bucket.this.id
+  description = "ID of the S3 bucket used to store Terraform state"
+  value       = module.s3.s3_bucket_id #aws_s3_bucket.this.id
 }
 
 output "s3_bucket_arn" {
-  value = module.s3.s3_bucket_arn #aws_s3_bucket.this.arn
+  description = "ARN of the S3 bucket used to store Terraform state"
+  value       = module.s3.s3_bucket_arn #aws_s3_bucket.this.arn
 }
 
 output "terraform_state_dynamodb_table_id" {
-  value = aws_dynamodb_table.this.id
+  description = "ID of the DynamoDB table used for Terraform state locking"
+  value       = aws_dynamodb_table.this.id
 }
